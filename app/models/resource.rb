1d4d55ae9de2b5033ec7027177fee868766a5439
amyhenning <henning.amy@gmail.com>
2018-06-13 04:23:38 +0000

    Set up model associations

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,6 @@
 class Resource < ApplicationRecord
 	enum type: { "Adoption Agencies": 0, "Books": 1, "DNA Testing": 2, "Events": 3, "Film, TV, and Music": 4, "KAD-Run Organizations": 5, "Local Groups": 6, "Tours": 7, "Other": 8 }
 
+	belongs_to :user
 	has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
 	belongs_to :resource
+	belongs_to :user
 end
