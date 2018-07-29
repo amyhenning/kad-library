092755cd4fa2e1184b54477fd153f3116a12b6ec
amyhenning <henning.amy@gmail.com>
2018-07-29 20:01:55 +0000

    Updated design of resource show pages, updated footer, removed box-shadows

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
 	def about
 	end
+
+	def start_here
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 	root 'static_pages#index'
 	get 'about', to: 'static_pages#about'
+	get 'start_here', to: 'static_pages#start_here'
 	resources :resources
 	resources :favorite_resources, only: [:create, :destroy]
 end
