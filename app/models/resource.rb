5ee80f2791cfecd1e9411b90ac6a990c106e2da8
amyhenning <henning.amy@gmail.com>
2018-07-30 04:29:20 +0000

    Added additional resource categories

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,5 @@
 class Resource < ApplicationRecord
-	enum resource_category: [ "Adoption Agency", "Book", "DNA Test", "Event", "Film, TV, and Music", "KAD-Run Organization", "Local Group", "Tour", "Other" ]
+	enum resource_category: [ "Adoption Agency", "Book", "DNA Test", "Event", "Film, TV, and Music", "KAD-Run Organization", "Local Group", "Tour", "Other", "Resources for Kids", "Travel Tips" ]
 
 	belongs_to :user
 	has_many :reviews
