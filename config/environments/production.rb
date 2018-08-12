cc752c5e752a44b238033e974e4ed66961c6af78
amyhenning <henning.amy@gmail.com>
2018-08-12 04:32:38 +0000

    fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'heroku.com'
+    domain: 'heroku.com',
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
     authentication: 'plain',
