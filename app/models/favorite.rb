cc52a0e57acb41936cdcceb85e699dde873fd739
amyhenning <henning.amy@gmail.com>
2018-09-14 01:45:45 +0000

    Created resource method in favorites and implemented it in the users#show view

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,10 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :favorited, polymorphic: true
+
+  def resource
+  	# if resource is nil, set it to what's on the right
+  	# the next time you call the method, it will be a valid resource (as long as it finds one)
+  	@resource ||= Resource.find_by_id(favorited_id)
+  end
 end
