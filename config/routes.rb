cbeaedc967fab757b212dd7015382ce9e389f494
amyhenning <henning.amy@gmail.com>
2018-11-27 19:50:32 +0000

    Updated static pages and corresponding footer links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 	root 'static_pages#index'
 	get 'about', to: 'static_pages#about'
 	get 'start_here', to: 'static_pages#start_here'
+	get 'faq', to: 'static_pages#faq'
+	get 'contact', to: 'static_pages#contact'
+	get 'contribute', to: 'static_pages#contribute'
 	resources :favorite_resources, only: [:create, :destroy]
 	resources :resources do
 		resources :reviews, only: [:create, :destroy]
